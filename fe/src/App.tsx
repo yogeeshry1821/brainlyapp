95ebd2394ad73b773814b817946dc3a800ab74cb
Yogeesh <yryadappa100@gmail.com>
2024-12-18 22:56:13 +0530

    updates

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect,useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [obj,setObj]=useState({
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    city: 'New York',
+    country: 'USA',
+    address:{
+      firstLine: 'John Street',
+      secondLine: 'Apt 123'
+    }
+  })
+  const handleClick=() => {
+    setObj({ ...obj, address: { ...obj.address,secondLine: 'apt 234' }});
+    console.log(obj.address.secondLine)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>hi</div>
+      <button onClick={handleClick}>click me</button>
+      <h1>{obj.firstName}</h1>
+      <h1>{obj.lastName}</h1>
+      <h1>{obj.age}</h1>
+      <h1>{obj.address.firstLine}</h1>
+      <h1>{obj.address.secondLine}</h1>
+
     </>
   )
 }
